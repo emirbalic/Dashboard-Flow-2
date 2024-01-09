4fd0372a4d75ffafe3b4008472fa4876953dbcff
emirbalic <emirbalic@gmail.com>
2024-01-09 16:21:37 +0100

    Browser API - UserDeactivateAction

--- src/views/administration/actions/UserDeactivateAction.vue
+++ src/views/administration/actions/UserDeactivateAction.vue
@@ -3,12 +3,12 @@
     <div class="admin-action-title">
       <h3>
         <b>
-          Are you sure you want to deactivate your profile? 
+          Are you sure you want to deactivate your profile?
         </b>
       </h3>
     </div>
     <div class="admin-action-content">
-    
+
       <div>
         <button class="button is-primary" @click="deactivateProfile">
           Deactivate
@@ -30,20 +30,45 @@ import { showNotice } from '@/visuals';
 
 
 export default defineComponent({
- 
+
   setup() {
 
- 
+
 
     const deactivateProfile = async () => {
 
-      deactivate().then(()=> {
-        removeFromStore('logged');
+      var status = confirm("Are you sure you want to do that?");
+      if (status == true) {
+        deactivate()
+          .then(() => {
+            removeFromStore('logged');
             //   api.cancel();
             router.push({
-                name: 'login',
+              name: 'login',
             });
-      });
+          }).then(() => {
+
+            showNotice({
+              props: {
+                type: 'success',
+                duration: 10000,
+                message: 'Your account is successfully deactivated, to re-activate contact administration.'
+              },
+            });
+          });
+      }
+      else {
+        // console.log("You pressed Cancel!");
+        showNotice({
+          props: {
+            type: 'success',
+            duration: 5000,
+            message: 'You made a wise choice ;)',
+          },
+        });
+      }
+
+
 
 
 
@@ -52,24 +77,24 @@ export default defineComponent({
       //         name: 'users-overview',
       //     });
 
-      if (!status) {
-        showNotice({
-          props: {
-            type: 'error',
-            duration: 5000,
-            // message: `The user ${body.username} can not be created at this time`,
-          },
-        });
-      } else {
-        // closeModal();
-        showNotice({
-          props: {
-            type: 'success',
-            duration: 5000,
-            // message: `The user ${body.username} successfully created`,
-          },
-        });
-      }
+      // if (!status) {
+      //   showNotice({
+      //     props: {
+      //       type: 'error',
+      //       duration: 5000,
+      //       // message: `The user ${body.username} can not be created at this time`,
+      //     },
+      //   });
+      // } else {
+      //   // closeModal();
+      //   showNotice({
+      //     props: {
+      //       type: 'success',
+      //       duration: 5000,
+      //       // message: `The user ${body.username} successfully created`,
+      //     },
+      //   });
+      // }
     };
 
 
